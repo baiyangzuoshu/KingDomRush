87f77d4d6edbfd324a9c4ff51d54940ba447b9f4
baiyangzuoshu <baiyangzuo_shu@aliyun.com>
2023-10-22 09:23:24 +0800

    init
    导入框架

---
+++ assets/Scripts/GameLanch .ts
@@ -0,0 +1,40 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
+
+import { EventManager } from "../FrameWork/manager/EventManager";
+import { ExcelManager } from "../FrameWork/manager/ExcelManager";
+import FGUIManager from "../FrameWork/manager/FGUIManager";
+import { ResManager } from "../FrameWork/manager/ResManager";
+import { ResManagerPro } from "../FrameWork/manager/ResManagerPro";
+import { UIManager } from "../FrameWork/manager/UIManager";
+import GameApp from "./GameApp";
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class GameLanch extends cc.Component {
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+         //框架
+         this.addComponent(FGUIManager)
+         this.addComponent(ExcelManager)
+         this.addComponent(EventManager)
+         this.addComponent(ResManager)
+         this.addComponent(UIManager)
+         this.addComponent(ResManagerPro)
+         //游戏
+         this.addComponent(GameApp)
+    }
+
+    start () {
+        GameApp.getInstance().startGame()
+    }
+
+    // update (dt) {}
+}
